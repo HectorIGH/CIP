78021189b25f0e7ad589b9db1d4dde779b576dfd
HectorIGH <47642123+HectorIGH@users.noreply.github.com>
2019-03-23 16:04:20 -0600

    Huge mods
    Relevant modifications to 1-NN function which improves execution time.

--- Main.py
+++ Main.py
@@ -5,6 +5,9 @@ from scipy.spatial import distance
 import datetime
 import validationMethods as vm
 import classifiers as cf
+import csv
+import smtplib
+import ssl
         
 ##### HOLD - OUT #####
 #HoldOut(percentageOfTraining, Seed, pathOfDB)
@@ -18,11 +21,11 @@ import classifiers as cf
 
 #p = float('inf')
 
-#uNN(pandaDataFrame, set of Training, set of Test, p value of Minkowski metric)
+#uNN(pandaDataFrame, set of Training, set of Test, numpy matrix of the training patterns)
 #Prints accuracy and then
 #Returns success count, len(P) and accuracy
 #suc, lenP accu = cf.uNN(df, E, P, 2)
-
+"""
 data = pd.read_csv("DB/iris.csv")
 df = pd.DataFrame(data)
 suc = 0
@@ -34,6 +37,51 @@ for i in range(patterns):
     suc += cf.uNN(df, E, P, 2)[0]
 print(datetime.datetime.now())
 print("Accuracy: ", suc/patterns)
+"""
+
+data = pd.read_csv("DB/letter-recognition.csv")
+df = pd.DataFrame(data)
+suc = 0
+patterns = len(df)
+print("Let's classify. Starting at:")
+print(datetime.datetime.now())
+##### LOOCV PART####
+E = [i for i in range(len(df))]
+
+mE = np.zeros((len(E), len(df.iloc[E[0]]) - 1))
+for i in range(len(E)):
+    for j in range(len(df.iloc[E[i]]) - 1):
+        mE[i][j] = df.iloc[E[i]][j]
+        
+for i in range(patterns):
+    e = E.copy()
+    del e[i]
+    me = np.delete(mE, i, 0)
+    suc += cf.uNN(df, e, [i], me)
+print(datetime.datetime.now())
+print("Accuracy: ", suc/patterns)
+
+
+port = 465
+password = ".1ErnoGauss1."
+smtp_server = "smtp.gmail.com"
+sender_email = "hi.garcia.hdez@gmail.com"
+receiver_email = "hi.garcia.hdez@gmail.com"
+message = str(suc/patterns)
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context = context) as server:
+    server.login("hi.garcia.hdez@gmail.com", password)
+    server.sendmail(sender_email, receiver_email, message)
+
+
+
+
+
+
+
+
+
+
 
 
 
--- classifiers.py
+++ classifiers.py
@@ -4,20 +4,16 @@ import random
 from scipy.spatial import distance
 import datetime
 
-def uNN(df, E, P, p):
+def uNN(df, E, P, mE):
     clase = -1
     success = 0
     #print(datetime.datetime.now())
-    mE = np.zeros((len(E), len(df.iloc[E[0]]) - 1))
-    for i in range(len(E)):
-        for j in range(len(df.iloc[E[i]]) - 1):
-            mE[i][j] = df.iloc[E[i]][j]
 
     #print("Let's classify. Starting at:")
     #print(datetime.datetime.now())
     for i in range(len(P)):
         a = np.array([df.iloc[P[i]][:-1]])
-        MDistance = distance.cdist(mE, a, 'minkowski', p)
+        MDistance = distance.cdist(mE, a, 'euclidean')
         #MDistance = distance.cdist(mE, a, 'chebyshev')
         clase = -1
         index = np.where(MDistance == np.amin(MDistance))
@@ -28,4 +24,4 @@ def uNN(df, E, P, p):
         #print(P[i], " belongs to class", clase, df.iloc[P[i]][-1])
     #print(datetime.datetime.now())
     #print("Accuracy: ", success/len(P), success, len(P))
-    return success, len(P), success/len(P)
+    return success
